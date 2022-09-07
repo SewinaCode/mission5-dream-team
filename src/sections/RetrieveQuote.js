de0d9f6c231235bf9b832db499778cb22fa19d52
nicolashayvice3 <nicolashayvice@gmail.com>
2022-09-08 11:19:59 +1200

    retrievequote footer

--- src/sections/RetrieveQuote.js
+++ src/sections/RetrieveQuote.js
@@ -6,7 +6,30 @@ export default function RetrieveQuote() {
   return (
     <>
         <Header />
-       <h1 className="text-yellow-500">hello world</h1>
+      <div className="relative">
+       <h2 className="text-black">hello world</h2>
+       <div className="bg-amber-600">
+
+       </div>
+       <footer class="pt-24 fixed w-full inset-x-0 bottom-0 pb-12 px-6 shadow md:flex md:items-center md:justify-between bg-sky-600">
+        <span class="text-sm text-white sm:text-center">© Copyright 2022 Company Name.
+        </span>
+        <ul class="flex flex-wrap items-center mt-3 sm:mt-0">
+            <li>
+                <p class="mr-4 text-sm text-white hover:underline md:mr-6">Privacy Policy</p>
+            </li>
+            <li>
+                <p class="mr-4 text-sm text-white hover:underline md:mr-6">Terms & Conditions</p>
+            </li>
+            <li>
+                <p class="mr-4 text-sm text-white hover:underline md:mr-6">Cookie Policy</p>
+            </li>
+            <li>
+                <p class="text-sm text-white hover:underline">Contact</p>
+            </li>
+        </ul>
+       </footer>
+      </div>
     </>
   );
 } 
