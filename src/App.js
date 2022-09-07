3c428e82d7c34436c3825452e4b61c54350516b8
nicolashayvice3 <nicolashayvice@gmail.com>
2022-09-07 19:00:10 +1200

    retrievequote route

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import "./App.css";
 import Header from "./sections/header";
 import Introduction from "./sections/Intro-Section";
 import MainNav from "./sections/mainNav";
+import RetrieveQuote from "./sections/RetrieveQuote"
 import Policies from "./sections/policy-sec";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <MainNav />
-      <Introduction />
-      <Policies />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <MainNav />
+        <Introduction />
+        <Policies />
+        <Switch>
+          <Route path="retrievequote">
+            <RetrieveQuote />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
