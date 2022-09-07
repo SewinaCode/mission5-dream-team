91bcd13b82281551846fda1519ce14d116e2c2e5
nicolashayvice3 <nicolashayvice@gmail.com>
2022-09-07 20:30:03 +1200

    react-router-dom trying out

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import "./App.css";
 import Header from "./sections/header";
 import Introduction from "./sections/Intro-Section";
 import MainNav from "./sections/mainNav";
-import RetrieveQuote from "./sections/RetrieveQuote/RetrieveQuote"
+import RetrieveQuote from "./sections/RetrieveQuote"
 import Policies from "./sections/policy-sec";
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <Switch>
-          <Route path="/">
-            <MainNav />
-            <Introduction />
-            <Policies />
-          </Route>
-          <Route path="retrievequote">
-            <RetrieveQuote />
-          </Route>
-        </Switch>
+        <Introduction />
+        <MainNav />
+        <Policies />
+        <Routes>
+          <Route exact path="retrievequote" element={<RetrieveQuote />} />
+        </Routes>
       </div>
     </Router>
   );
---
+++ src/sections/RetrieveQuote.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import Buttons from "../components/Buttons/Buttons";
+// import { RedBorderBtn } from "../components/Buttons/variants/variants";
+
+
+export default function RetrieveQuote() {
+  // const handleClick = () => {
+  //   alert("button clicked");
+  // };
+  console.log("hi i am here")
+  
+  return (
+    <>
+       <h1>hello world</h1>
+    </>
+  );
+} 
