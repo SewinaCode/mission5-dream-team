03a6f47e3668496786f1e82eaf34af48804f339f
nicolashayvice3 <nicolashayvice@gmail.com>
2022-09-07 19:06:06 +1200

    components route home

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./sections/header";
 import Introduction from "./sections/Intro-Section";
 import MainNav from "./sections/mainNav";
-import RetrieveQuote from "./sections/RetrieveQuote"
+import RetrieveQuote from "./sections/RetrieveQuote/RetrieveQuote"
 import Policies from "./sections/policy-sec";
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
@@ -11,10 +11,12 @@ function App() {
     <Router>
       <div className="App">
         <Header />
-        <MainNav />
-        <Introduction />
-        <Policies />
         <Switch>
+          <Route path="/">
+            <MainNav />
+            <Introduction />
+            <Policies />
+          </Route>
           <Route path="retrievequote">
             <RetrieveQuote />
           </Route>
