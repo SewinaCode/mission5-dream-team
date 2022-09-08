ecb9c49ac5f3de2ff6fff4284f626b48cc1da319
Sellyna <sellynalky@gmail.com>
2022-09-08 19:21:15 +1200

    faq section added

---
+++ src/components/faq-comp/faq.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../../components/card-comp.css";
+import "../../components/sec-heading.css";
+import "./faq.css";
+import "./what-can-help.css";
+import "../../components/Buttons/button.css";
+import { ParaText } from "../sec-heading";
+import { BlackText } from "../variants-comp/comp-variants";
+import RedArrow from "../../assets/red-dropdown-arrow.png";
+import LongerLine from "../../assets/Line 18.png";
+
+function Questions({ text }) {
+  return (
+    <div className="feat-height ">
+      <div className="faq-ques-spacing can-help-align">
+        <ParaText
+          className="feature-text sml-weight bt-font-header card-height"
+          variant={BlackText}
+          text={text}
+        />
+        <img className="arrow" src={RedArrow} alt="red-arrow" />
+      </div>
+      <img
+        className="padding-top-bottom"
+        src={LongerLine}
+        alt="faq-long-line"
+      />
+    </div>
+  );
+}
+
+export default Questions;
--- src/sections/faq-section.js
+++ src/sections/faq-section.js
@@ -1,23 +1,36 @@
 import React from "react";
-import "../components/sec-heading.css";
 import "../components/card-comp.css";
-import { LightBlue } from "../components/variants-comp/comp-variants";
+import "../components/sec-heading.css";
+import "../components/faq-comp/what-can-help.css";
+import {
+  LightBlue,
+  WhiteBG,
+  BlueText,
+} from "../components/variants-comp/comp-variants";
+import { HeadingTxt } from "../components/sec-heading";
 import WhatHelp from "../components/faq-comp/what-can-help";
-// import HelpFeatHeading from "../components/faq-comp/help-feat";
-
-// //Image import
-// import AccidentClaims from "../assets/accident_claims.png";
+import Questions from "../components/faq-comp/faq";
 
 export default function FAQ() {
   return (
     <div className="general-padding" style={LightBlue}>
       <WhatHelp />
-      {/* <HelpFeatHeading
-        classname="section-link bt-font-header med-weight card-height can-help-column can-help-baseline"
-        variant={DarkBlueText}
-        text="Accidents Claims"
-        image={AccidentClaims}
-      /> */}
+      <section className="general-padding med-margin-18" style={LightBlue} />
+      <div style={WhiteBG}>
+        <HeadingTxt
+          className="section-header med-weight med-font-header card-height pad-top-big "
+          variant={BlueText}
+          text="Frequently Asked Questions"
+        />
+        <div className="dropdown-ques-padding" style={WhiteBG}>
+          <Questions text="If I need to cancel my policy, can I get a refund ?" />
+          <Questions text="Who is the underwriter ?" />
+          <Questions text="I’ve moved house. How do I update my policy address with you?" />
+          <Questions text="How do I make a car insurance claim ?" />
+          <Questions text="Someone drove into my car, what do I do ? " />
+        </div>
+        <section className="padding-top-bottom " style={WhiteBG} />
+      </div>
     </div>
   );
 }
