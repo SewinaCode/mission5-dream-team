521a74fe1278ef0a8c3c0ff56469638c8a96e8cb
Sellyna <sellynalky@gmail.com>
2022-09-09 17:14:02 +1200

    issues fixed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,10 @@
-import FAQ from "../sections/Faq-section";
-import Header from "../sections/Header";
+import FAQ from "../sections/faq-section";
+import Header from "../sections/header";
 import Introduction from "../sections/Intro-Section";
 import MainNav from "../sections/mainNav";
 import Policies from "../sections/policy-sec";
 import TestimonySection from "../sections/testimonies";
 import { WhyChooseT } from "../sections/why-choose-t";
-
 
 export default function Home() {
   return (
